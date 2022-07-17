c24f15b66df6bc65ff0e8bc2c3919d5574409f6a
learngodo <ashirrashid00@gmail.com>
2022-07-17 10:50:38 +0500

    changed Label.gd

--- Label.gd
+++ Label.gd
@@ -2,4 +2,7 @@ extends Label
 
 
 func _on_TextEdit_text_changed():
-	self.text = get_parent().get_node("TextEdit").text
+	var updated_text = get_parent().get_node("TextEdit").text
+	self.text = "The Text was changed to '{new_text}'".format({
+		new_text = updated_text
+		})
